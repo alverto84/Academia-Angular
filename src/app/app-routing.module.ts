8d17b3438a055d29108e6acea6af69e955e73503
albertdgz <albertodominguez@gmail.com>
2020-08-09 01:53:14 +0200

    Inicio primer formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { CursosComponent } from './components/cursos/cursos.component';
 import { ExpressionStatement } from '@angular/compiler';
 import { ExamenesComponent } from './components/examenes/examenes.component';
+import { AlumnosFormComponent } from './components/alumnos/alumnos-form.component';
 
 const routes: Routes = [
   {path : '', pathMatch : 'full', redirectTo : 'cursos'},
   {path : 'alumnos', component : AlumnosComponent},
+  {path : 'alumnos-form', component : AlumnosFormComponent},
   {path : 'cursos', component : CursosComponent},
   {path : 'examenes', component : ExamenesComponent}
 ];
---
+++ src/app/components/alumnos/alumnos-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-form',
+  templateUrl: './alumnos-form.component.html',
+  styleUrls: ['./alumnos-form.component.css']
+})
+export class AlumnosFormComponent implements OnInit {
+
+  titulo= 'Crear alumnos';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
