814f9f45445a40c3e3ac129aafc5b7f40fdbf5fd
alverto84 <albertodominguezalonso.com>
2020-08-09 19:49:22 +0200

    Mensajes alert con sweetalert2 para alumnos

--- src/app/components/alumnos/alumnos-form.component.ts
+++ src/app/components/alumnos/alumnos-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/models/alumno';
+import { AlumnoService } from 'src/app/services/alumno.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-alumnos-form',
@@ -8,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
 export class AlumnosFormComponent implements OnInit {
 
   titulo= 'Crear alumnos';
+  alumno: Alumno = new Alumno();
+  error: any;
 
-  constructor() { }
+  constructor(private service: AlumnoService, 
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id: number = +params.get('id');
+        if(id){
+        this.service.ver(id).subscribe(alumno => this.alumno = alumno)
+      }
+    });
   }
 
+  public crear(): void { 
+    this.service.crear(this.alumno).subscribe(alumno => {
+      console.log(`Alumno ${alumno.nombre} creado con éxito`); 
+      Swal.fire(`Nuevo:`,`Alumno ${alumno.nombre} creado con éxito`, 'success');
+      this.router.navigate(['/alumnos']);
+    }, err => {
+      if(err.status === 400){
+        this.error = err.error;
+        console.log(this.error);
+      }
+    });
+  }
+
+  public editar(): void { 
+    this.service.editar(this.alumno).subscribe(alumno => {
+      console.log(`Alumno ${alumno.nombre} actualizado con éxito`); 
+      Swal.fire(`Modificado: `, `Alumno ${alumno.nombre} actualizado con éxito`, 'success');
+      this.router.navigate(['/alumnos']);
+    }, err => {
+      if(err.status === 400){
+        this.error = err.error;
+        console.log(this.error);
+      }
+    });
+  }
 }
--- src/app/components/alumnos/alumnos.component.ts
+++ src/app/components/alumnos/alumnos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlumnoService } from 'src/app/services/alumno.service';
 import { Alumno } from 'src/app/models/alumno';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-alumnos',
@@ -16,5 +17,25 @@ export class AlumnosComponent implements OnInit {
 
   ngOnInit(): void {
     this.service.listar().subscribe(alumnos => this.alumnos = alumnos);
+  }
+
+  public eliminar(alumno: Alumno): void {
+
+    Swal.fire({
+      title: `Cuidado:`,
+      text: `¿Seguro que desea eliminar al alumno ${alumno.nombre}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.value) {
+        this.service.eliminar(alumno.id).subscribe(() => {
+          this.alumnos = this.alumnos.filter(a => a !== alumno);
+        })
+        Swal.fire(`Eliminado: `,`Alumno ${alumno.nombre} eliminado con éxito`, 'success');
+      }
+    });
   }
 }
